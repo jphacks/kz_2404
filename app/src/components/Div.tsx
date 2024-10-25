4ce3419b31d6d624e2adc15afd28aca89c58de28
hikahana <22.h.hanada.nutfes@gmail.com>
2024-10-25 12:44:42 +0000

    feat firebase作成

---
+++ app/src/components/Div.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_APPID
+};
+
+const provider = new GoogleAuthProvider();
+async function signUp() {
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      console.log(user);
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+export default function Div() {
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    return (
+        <div>
+            <h1>Div</h1>
+            <button onClick={signUp}>aaaaa</button>
+        </div>
+    )
+}
