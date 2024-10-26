e646e1a9cf30081deb67fb65914fab4a2d3d8942
harata-t <22.t.harata.nutfes@gmail.com>
2024-10-26 16:10:46 +0900

    dailyをスコアでソート

--- app/src/components/view/ranking/RankingListToday.tsx
+++ app/src/components/view/ranking/RankingListToday.tsx
@@ -4,55 +4,58 @@ interface RankingListTodayProps {
   }
   
   export default function RankingListToday({ selectedTopic }: RankingListTodayProps) {
-    const allData: { [key: string]: { rank: number; name: string; time: string; similarity: string; score: string; image: string; }[] } = {
+    const allData: { [key: string]: { id: string; name: string; time: string; similarity: string; score: string; image: string; }[] } = {
       'お題1': [
-        { rank: 1, name: 'yama', time: '40.0秒', similarity: '90%', score: '95点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 2, name: 'taro', time: '45.0秒', similarity: '80%', score: '90点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 3, name: 'hanako', time: '50.0秒', similarity: '70%', score: '85点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 4, name: 'jiro', time: '55.0秒', similarity: '60%', score: '80点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 5, name: 'saburo', time: '60.0秒', similarity: '50%', score: '75点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 6, name: 'shiro', time: '65.0秒', similarity: '40%', score: '70点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 7, name: 'goro', time: '70.0秒', similarity: '30%', score: '65点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 8, name: 'yama', time: '75.0秒', similarity: '20%', score: '60点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 9, name: 'taro', time: '80.0秒', similarity: '10%', score: '55点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 10, name: 'hanako', time: '85.0秒', similarity: '5%', score: '50点', image: 'https://placehold.jp/150x150.png' },
+        { id: '1', name: 'yama', time: '40.0秒', similarity: '90%', score: '95点', image: 'https://placehold.jp/150x150.png' },
+        { id: '2', name: 'taro', time: '45.0秒', similarity: '80%', score: '90点', image: 'https://placehold.jp/150x150.png' },
+        { id: '3', name: 'hanako', time: '50.0秒', similarity: '70%', score: '85点', image: 'https://placehold.jp/150x150.png' },
+        { id: '4', name: 'jiro', time: '55.0秒', similarity: '60%', score: '80点', image: 'https://placehold.jp/150x150.png' },
+        { id: '5', name: 'saburo', time: '60.0秒', similarity: '50%', score: '75点', image: 'https://placehold.jp/150x150.png' },
+        { id: '6', name: 'shiro', time: '65.0秒', similarity: '40%', score: '70点', image: 'https://placehold.jp/150x150.png' },
+        { id: '7', name: 'goro', time: '70.0秒', similarity: '30%', score: '65点', image: 'https://placehold.jp/150x150.png' },
+        { id: '8', name: 'yama', time: '75.0秒', similarity: '20%', score: '60点', image: 'https://placehold.jp/150x150.png' },
+        { id: '9', name: 'taro', time: '80.0秒', similarity: '10%', score: '55点', image: 'https://placehold.jp/150x150.png' },
+        { id: '10', name: 'hanako', time: '85.0秒', similarity: '5%', score: '50点', image: 'https://placehold.jp/150x150.png' },
         // 他のデータ
       ],
       'お題2': [
-        { rank: 1, name: 'jiro', time: '50.0秒', similarity: '70%', score: '85点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 2, name: 'saburo', time: '55.0秒', similarity: '60%', score: '80点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 3, name: 'shiro', time: '60.0秒', similarity: '50%', score: '75点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 4, name: 'goro', time: '65.0秒', similarity: '40%', score: '70点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 5, name: 'yama', time: '70.0秒', similarity: '30%', score: '65点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 6, name: 'taro', time: '75.0秒', similarity: '20%', score: '60点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 7, name: 'hanako', time: '80.0秒', similarity: '10%', score: '55点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 8, name: 'jiro', time: '85.0秒', similarity: '5%', score: '50点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 9, name: 'saburo', time: '90.0秒', similarity: '1%', score: '45点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 10, name: 'shiro', time: '95.0秒', similarity: '0%', score: '40点', image: 'https://placehold.jp/150x150.png' },
+        { id: '1', name: 'jiro', time: '50.0秒', similarity: '70%', score: '85点', image: 'https://placehold.jp/150x150.png' },
+        { id: '2', name: 'saburo', time: '55.0秒', similarity: '60%', score: '80点', image: 'https://placehold.jp/150x150.png' },
+        { id: '3', name: 'shiro', time: '60.0秒', similarity: '50%', score: '75点', image: 'https://placehold.jp/150x150.png' },
+        { id: '4', name: 'goro', time: '65.0秒', similarity: '40%', score: '70点', image: 'https://placehold.jp/150x150.png' },
+        { id: '5', name: 'yama', time: '70.0秒', similarity: '30%', score: '65点', image: 'https://placehold.jp/150x150.png' },
+        { id: '6', name: 'taro', time: '75.0秒', similarity: '20%', score: '60点', image: 'https://placehold.jp/150x150.png' },
+        { id: '7', name: 'hanako', time: '80.0秒', similarity: '10%', score: '55点', image: 'https://placehold.jp/150x150.png' },
+        { id: '8', name: 'jiro', time: '85.0秒', similarity: '5%', score: '50点', image: 'https://placehold.jp/150x150.png' },
+        { id: '9', name: 'saburo', time: '90.0秒', similarity: '1%', score: '45点', image: 'https://placehold.jp/150x150.png' },
+        { id: '10', name: 'shiro', time: '95.0秒', similarity: '0%', score: '40点', image: 'https://placehold.jp/150x150.png' },
         // 他のデータ
       ],
       'お題3': [
-        { rank: 1, name: 'shiro', time: '60.0秒', similarity: '50%', score: '75点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 2, name: 'goro', time: '65.0秒', similarity: '40%', score: '70点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 3, name: 'yama', time: '70.0秒', similarity: '30%', score: '65点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 4, name: 'taro', time: '75.0秒', similarity: '20%', score: '60点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 5, name: 'hanako', time: '80.0秒', similarity: '10%', score: '55点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 6, name: 'jiro', time: '85.0秒', similarity: '5%', score: '50点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 7, name: 'saburo', time: '90.0秒', similarity: '1%', score: '45点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 8, name: 'shiro', time: '95.0秒', similarity: '0%', score: '40点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 9, name: 'goro', time: '100.0秒', similarity: '0%', score: '35点', image: 'https://placehold.jp/150x150.png' },
-        { rank: 10, name: 'yama', time: '105.0秒', similarity: '0%', score: '30点', image: 'https://placehold.jp/150x150.png' },
+        { id: '1', name: 'shiro', time: '60.0秒', similarity: '50%', score: '75点', image: 'https://placehold.jp/150x150.png' },
+        { id: '2', name: 'goro', time: '65.0秒', similarity: '40%', score: '70点', image: 'https://placehold.jp/150x150.png' },
+        { id: '3', name: 'yama', time: '70.0秒', similarity: '30%', score: '65点', image: 'https://placehold.jp/150x150.png' },
+        { id: '4', name: 'taro', time: '75.0秒', similarity: '20%', score: '60点', image: 'https://placehold.jp/150x150.png' },
+        { id: '5', name: 'hanako', time: '80.0秒', similarity: '10%', score: '55点', image: 'https://placehold.jp/150x150.png' },
+        { id: '6', name: 'jiro', time: '85.0秒', similarity: '5%', score: '50点', image: 'https://placehold.jp/150x150.png' },
+        { id: '7', name: 'saburo', time: '90.0秒', similarity: '1%', score: '45点', image: 'https://placehold.jp/150x150.png' },
+        { id: '8', name: 'shiro', time: '95.0秒', similarity: '0%', score: '40点', image: 'https://placehold.jp/150x150.png' },
+        { id: '9', name: 'goro', time: '100.0秒', similarity: '0%', score: '35点', image: 'https://placehold.jp/150x150.png' },
+        { id: '10', name: 'yama', time: '105.0秒', similarity: '0%', score: '30点', image: 'https://placehold.jp/150x150.png' },
         // 他のデータ
       ],
     };
   
     const data = allData[selectedTopic] || [];
   
+    // scoreが高い順にソート
+    const sortedData = data.sort((a, b) => parseInt(b.score) - parseInt(a.score));
+  
     return (
       <div className="mt-4 space-y-4">
-        {data.map((item, index) => {
+        {sortedData.map((item, index) => {
           let bgColor;
-          switch (item.rank) {
+          switch (index + 1) {
             case 1:
               bgColor = 'bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600';
               break;
@@ -67,9 +70,9 @@ interface RankingListTodayProps {
           }
   
           return (
-            <div key={index} className="relative flex items-center bg-orange-100 rounded-lg shadow">
+            <div key={item.id} className="relative flex items-center bg-orange-100 rounded-lg shadow">
               <div className={`absolute -top-1 -left-1 ${bgColor} text-white text-base rounded-br-lg shadow-lg`} style={{ clipPath: 'polygon(0 0, 100% 0, 0 100%)', width: '50px', height: '50px', display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '3px' }}>
-                {item.rank}位
+                {index + 1}位
               </div>
               <img src={item.image} alt={item.name} className="w-20 h-20 object-cover rounded ml-0" />
               <div className="m-2 flex flex-col w-1/5">
@@ -92,4 +95,3 @@ interface RankingListTodayProps {
       </div>
     );
   }
-  
