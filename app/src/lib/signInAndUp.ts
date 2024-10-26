dc1787e010a5d26884d9350e50cbb8435dfc7188
nose221834 <iatyy01151821@gmail.com>
2024-10-27 05:11:31 +0900

    Revert "GPTの修正"
    This reverts commit a8d10ceb7f7022c8f7984720f216e4aad176757c.

--- app/src/lib/signInAndUp.ts
+++ app/src/lib/signInAndUp.ts
@@ -1,44 +1,37 @@
 import type { User } from "@firebase/auth";
 
 export const signInOrUp = async (user: User) => {
-  try {
-    const res = await fetch(`/api/user/?uid=${user.uid}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
+  await fetch(`/api/user/?uid=${user.uid}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
     if (res.status === 200) {
       storeStorageUser(user.uid);
       toRoot();
-    } else {
-      await signUp(user); // 応答を待つ
+      return;
     }
-  } catch (error) {
-    console.error("エラーが発生しました:", error);
-  }
+
+    signUp(user);
+  });
 };
 
 const signUp = async (user: User) => {
-  try {
-    const res = await fetch("/api/user/new", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    });
-
+  await fetch("/api/user/new", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  }).then((res) => {
     if (res.status === 200) {
       storeStorageUser(user.uid);
       toRoot();
     } else {
       throw new Error("ユーザー登録に失敗");
     }
-  } catch (error) {
-    console.error("エラーが発生しました:", error);
-  }
+  });
 };
 
 const storeStorageUser = (uid: string) => {
