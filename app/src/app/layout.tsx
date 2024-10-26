080321c5c4052f7865917a2514ca93d8597b3d0f
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-10-26 13:51:43 +0900

    [feat] フォントを追加

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
+const notoSansJP = Noto_Sans_JP({
+	weight: ["400", "700"],
+	subsets: ["latin"],
+	variable: "--font-noto-sans-jp",
+	display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,12 +20,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
+		<html lang="ja">
+			<body className={`${notoSansJP.className}`}>{children}</body>
 		</html>
 	);
 }
