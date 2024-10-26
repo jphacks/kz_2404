71e098d6b34324eb3883631f2d7010cd391caebe
hikahana <22.h.hanada.nutfes@gmail.com>
2024-10-26 13:21:55 +0000

    fix biome check

--- app/src/app/api/score/week/route.ts
+++ app/src/app/api/score/week/route.ts
@@ -1,17 +1,17 @@
 import { prisma } from "@lib/prisma";
-import { subDays, getDay } from 'date-fns';
+import { getDay, subDays } from "date-fns";
 
 const getLastSunday = (date: Date) => {
-    const dayOfWeek = getDay(date);
-    return subDays(date, dayOfWeek);
-}
+	const dayOfWeek = getDay(date);
+	return subDays(date, dayOfWeek);
+};
 
 export async function GET() {
-    const today = new Date();
-    const lastSunday = getLastSunday(today);
+	const today = new Date();
+	const lastSunday = getLastSunday(today);
 
-    const startOfDay = new Date(lastSunday.setHours(0, 0, 0, 0));
-    const endOfDay = new Date(today.setHours(23, 59, 59, 999));
+	const startOfDay = new Date(lastSunday.setHours(0, 0, 0, 0));
+	const endOfDay = new Date(today.setHours(23, 59, 59, 999));
 
 	const scores = await prisma.score.findMany({
 		where: {
@@ -20,50 +20,42 @@ export async function GET() {
 				lte: endOfDay,
 			},
 		},
-        orderBy: {
-            id: "asc"
-        },
-        select: {
-            id: true,
-            point: true,
-            answerTime: true,
-            similarity: true,
-            imageUrl: true,
-            createdAt: true,
-            updatedAt: true,
-            user: {
-                select: {
-                    uid: true,
-                    name: true,
-                    email: true,
-                    photoUrl: true,
-                }
-            },
-            assignment: {
-                select: {
-                    wordId: true,
-                    date: true,
-                    word: {
-                        select: {
-                            english: true,
-                            japanese: true,
-                            difficulty: true
-                        }
-                    }
-                }
-            }
-        }
-		// include: {
-		// 	user: true,
-		// 	assignment: {
-		// 		include: {
-		// 			word: true,
-		// 		},
-		// 	},
-		// },
+		orderBy: {
+			id: "asc",
+		},
+		select: {
+			id: true,
+			point: true,
+			answerTime: true,
+			similarity: true,
+			imageUrl: true,
+			createdAt: true,
+			updatedAt: true,
+			user: {
+				select: {
+					uid: true,
+					name: true,
+					email: true,
+					photoUrl: true,
+				},
+			},
+			assignment: {
+				select: {
+					wordId: true,
+					date: true,
+					word: {
+						select: {
+							english: true,
+							japanese: true,
+							difficulty: true,
+						},
+					},
+				},
+			},
+		},
 	});
 
-    // 空の場合、空配列で返す
+	// 空の場合、空配列で返す
 	if (scores.length === 0) {
 		return new Response(JSON.stringify([]), {
 			status: 200,
