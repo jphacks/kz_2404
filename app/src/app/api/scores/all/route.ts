b34da5f57d28fe76b8823e21e5f38a1089c3c55e
hikahana <22.h.hanada.nutfes@gmail.com>
2024-10-26 22:54:02 +0000

    fix scores/all

--- app/src/app/api/scores/all/route.ts
+++ app/src/app/api/scores/all/route.ts
@@ -2,52 +2,32 @@ import type { RankingScores } from "@/types";
 import { prisma } from "@lib/prisma";
 
 export async function GET() {
-	const scores = await prisma.score.findMany({
-		include: {
-			user: true,
-			assignment: {
-				include: {
-					word: true,
-				},
-			},
-		},
-	});
-
 	const userScores = await prisma.user.findMany({
 		select: {
 			id: true,
 			name: true,
+			uid: true,
+			photoUrl: true,
 			scores: {
 				select: {
 					point: true,
 				},
 			},
 		},
+		distinct: ['uid'],
 	});
 
-	const scoreDetails: RankingScores[] = scores
-		.map((score) => {
-			// ユーザーの合計ポイント
-			const user = userScores.find((user) => user.id === score.id);
-			const sumScore = user ? user.scores.reduce((sum, score) => sum + score.point, 0) : 0;
+	const scoreDetails: RankingScores[] = userScores.map((userScore) => {
+		const sumScore = userScore.scores.reduce((sum, score) => sum + score.point, 0);
 
-			const rankingScore: RankingScores = {
-				id: score.id,
-				userName: score.user?.name || "",
-				imageUrl: score.imageUrl,
-				totalPoint: sumScore,
-			};
-			return rankingScore;
-		})
-		.sort((a, b) => b.totalPoint - a.totalPoint);
-
-	// 空の場合、空配列で返す
-	if (!scores) {
-		return new Response(JSON.stringify([]), {
-			status: 200,
-			headers: { "Content-Type": "application/json" },
-		});
-	}
+		const rankingScore: RankingScores = {
+			id: userScore.id,
+			userName: userScore?.name || "",
+			imageUrl: userScore.photoUrl,
+			totalPoint: sumScore,
+		};
+		return rankingScore;
+	}).sort((a, b) => b.totalPoint - a.totalPoint)
 
 	return new Response(JSON.stringify(scoreDetails), {
 		status: 200,
