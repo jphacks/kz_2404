c39f7d5183c039956dc373fba1974b740d0190bc
hikahana <22.h.hanada.nutfes@gmail.com>
2024-10-26 13:20:17 +0000

    fix format the responses

--- app/src/app/api/scores/all/route.ts
+++ app/src/app/api/scores/all/route.ts
@@ -3,21 +3,50 @@ import { prisma } from "@lib/prisma";
 export async function GET() {
 
 	const scores = await prisma.score.findMany({
-		include: { user: true, assignment: {
-			include: {
-				word: true
-			}
-		} },
+		orderBy: {
+            id: "asc"
+        },
+        select: {
+            id: true,
+            point: true,
+            answerTime: true,
+            similarity: true,
+            imageUrl: true,
+            createdAt: true,
+            updatedAt: true,
+            user: {
+                select: {
+                    uid: true,
+                    name: true,
+                    email: true,
+                    photoUrl: true,
+                }
+            },
+            assignment: {
+                select: {
+                    wordId: true,
+                    date: true,
+                    word: {
+                        select: {
+                            english: true,
+                            japanese: true,
+                            difficulty: true
+                        }
+                    }
+                }
+            }
+        }
 	});
 
+	// 空の場合、空配列で返す
 	if (!scores) {
-		return new Response(JSON.stringify({ message: "Not Found" }), {
-			status: 404,
+		return new Response(JSON.stringify([]), {
+			status: 200,
 			headers: { "Content-Type": "application/json" },
 		});
 	}
 
-	return new Response(JSON.stringify(scores), {
+	return new Response(JSON.stringify(scores, null, 2), {
 		status: 200,
 		headers: { "Content-Type": "application/json" },
 	});
