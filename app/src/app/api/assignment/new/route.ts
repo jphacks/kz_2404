14d15063247da2affc7ddfb8333b983f63d65e0e
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-10-27 02:33:00 +0900

    課題作成

---
+++ app/src/app/api/assignment/new/route.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest } from "next";
+import { prisma } from "@lib/prisma";
+import type { Score, ScoreDetail, Word } from "@/types";
+
+// 課題新規作成
+export async function POST(req: NextApiRequest) {
+	try {
+		const words = await prisma.word.findMany();
+		//ランダムでwordを取得
+		const word = words[Math.floor(Math.random() * words.length)];
+
+		// 課題を作成
+		const assignment = await prisma.assignment.create({
+			data: {
+				wordId: word.id,
+				date: new Date(),
+			},
+		});
+
+		return new Response(JSON.stringify({message: "成功"}), {
+			status: 200,
+			headers: { "Content-Type": "application/json" },
+		});
+	} catch (error) {
+		console.error(error);
+		return new Response(JSON.stringify({ message: "エラー" }), {
+			status: 500,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
+}
+
+
