e683d6cdb456eb24286e372f9ab610ede0ab35f5
hikahana <22.h.hanada.nutfes@gmail.com>
2024-10-26 13:22:46 +0000

    fix biome check

--- app/src/app/api/scores/all/route.ts
+++ app/src/app/api/scores/all/route.ts
@@ -1,41 +1,40 @@
 import { prisma } from "@lib/prisma";
 
 export async function GET() {
-
 	const scores = await prisma.score.findMany({
 		orderBy: {
-            id: "asc"
-        },
-        select: {
-            id: true,
-            point: true,
-            answerTime: true,
-            similarity: true,
-            imageUrl: true,
-            createdAt: true,
-            updatedAt: true,
-            user: {
-                select: {
-                    uid: true,
-                    name: true,
-                    email: true,
-                    photoUrl: true,
-                }
-            },
-            assignment: {
-                select: {
-                    wordId: true,
-                    date: true,
-                    word: {
-                        select: {
-                            english: true,
-                            japanese: true,
-                            difficulty: true
-                        }
-                    }
-                }
-            }
-        }
+			id: "asc",
+		},
+		select: {
+			id: true,
+			point: true,
+			answerTime: true,
+			similarity: true,
+			imageUrl: true,
+			createdAt: true,
+			updatedAt: true,
+			user: {
+				select: {
+					uid: true,
+					name: true,
+					email: true,
+					photoUrl: true,
+				},
+			},
+			assignment: {
+				select: {
+					wordId: true,
+					date: true,
+					word: {
+						select: {
+							english: true,
+							japanese: true,
+							difficulty: true,
+						},
+					},
+				},
+			},
+		},
 	});
 
 	// 空の場合、空配列で返す
