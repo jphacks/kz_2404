68ba3deb244ca2df88ecdd4c855c9b857989c4ae
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-10-27 11:00:15 +0900

    コード修正

--- app/src/app/api/assignment/new/route.ts
+++ app/src/app/api/assignment/new/route.ts
@@ -1,28 +1,27 @@
-import type { NextApiRequest } from "next";
-import { prisma } from "@lib/prisma";
-import type { Score, ScoreDetail, Word } from "@/types";
+;import { prisma } from "@lib/prisma";
+import type { Assignment } from "@/types";
+
 
 // 課題新規作成（ランダム）
-export async function POST(req: NextApiRequest) {
+export async function POST() {
 	try {
 		const words = await prisma.word.findMany();
 		//ランダムでwordを取得
 		const word = words[Math.floor(Math.random() * words.length)];
 
 		// 課題を作成
-		const assignment = await prisma.assignment.create({
+		const assignment: Assignment = await prisma.assignment.create({
 			data: {
 				wordId: word.id,
 				date: new Date(),
 			},
 		});
 
-		return new Response(JSON.stringify({message: "成功"}), {
+		return new Response(JSON.stringify(assignment), {
 			status: 200,
 			headers: { "Content-Type": "application/json" },
 		});
 	} catch (error) {
-		console.error(error);
 		return new Response(JSON.stringify({ message: "エラー" }), {
 			status: 500,
 			headers: { "Content-Type": "application/json" },
