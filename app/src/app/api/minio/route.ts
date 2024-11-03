148aec930562f122f99b632293ad0ef279c8501b
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-11-03 17:36:37 +0900

    kubosaka

---
+++ app/src/app/api/minio/route.ts
@@ -0,0 +1,76 @@
+import fs from 'fs';
+import { formidable, IncomingForm } from 'formidable';
+import type { NextRequest } from "next/server";
+import type { IncomingMessage } from 'http';
+import { Readable } from 'stream';
+import { Client } from 'minio';
+
+const minioClient = new Client({
+  endPoint: process.env.NEXT_PUBLIC_ENDPOINT || '',
+  port: Number(process.env.NEXT_PUBLIC_PORT),
+  accessKey: process.env.NEXT_PUBLIC_ACCESS_KEY || '',
+  secretKey: process.env.NEXT_PUBLIC_SECRET_KEY || '',
+  useSSL: false,
+});
+
+const BUCKET_NAME = 'kz2404';
+
+// NextRequestをIncomingMessageのようにラップする関数
+function toIncomingMessage(request: NextRequest): IncomingMessage {
+  const readable = new Readable({
+    read() {
+      request.body?.getReader().read().then(({ done, value }) => {
+        if (done) {
+          this.push(null);
+        } else {
+          this.push(Buffer.from(value));
+        }
+      });
+    },
+  });
+
+  const msg = Object.assign(readable, {
+    headers: Object.fromEntries(request.headers),
+    method: request.method,
+    url: request.nextUrl.pathname,
+  });
+
+  return msg as IncomingMessage;
+}
+
+
+export async function POST(req: NextRequest) {
+  const form = formidable();
+  const incomingMessage = toIncomingMessage(req);
+
+  form.parse(incomingMessage, async (err: any, fields: any, files: any) => {
+    if (err) {
+      throw new Error('Error parsing form');
+    }
+    const image = files.image[0];
+    const mimetype = image.type;
+    const metaData = {
+      'Content-Type': mimetype
+    };
+
+    try {
+      const response = await minioClient.putObject(
+        BUCKET_NAME,
+        image.originalFilename,
+        fs.createReadStream(image.filepath),
+        image.size,
+        metaData,
+      );
+    } catch (err) {
+      return new Response(JSON.stringify({ message: '失敗' }), {
+        status: 400,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+  });
+
+  return new Response(JSON.stringify({ message: '成功' }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
