f083bbd55993388c0fe9aaed0af580e9e0588a0d
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-11-10 04:56:03 +0900

    [feat] ナビゲーションのデザインを変更

--- app/src/components/view/ranking/TabNavigation.tsx
+++ app/src/components/view/ranking/TabNavigation.tsx
@@ -1,23 +1,28 @@
-// app/ranking/components/TabNavigation.tsx
-interface TabNavigationProps {
-    selectedTab: 'today' | 'weekly' | 'allTime';
-    setSelectedTab: (tab: 'today' | 'weekly' | 'allTime') => void;
-  }
+import { Button } from "@/components/ui/button";
 
-  export default function TabNavigation({ selectedTab, setSelectedTab }: TabNavigationProps) {
-    return (
-      <div className="flex space-x-4 p-2 bg-white rounded-md shadow-md">
-        {['today', 'weekly', 'allTime'].map((tab) => (
-          <button
-            key={tab}
-            onClick={() => setSelectedTab(tab as 'today' | 'weekly' | 'allTime')}
-            className={`flex-1 py-2 ${
-              selectedTab === tab ? 'bg-orange-200' : 'bg-gray-100'
-            } text-center rounded-md`}
-          >
-            {tab === 'today' ? '今日' : tab === 'weekly' ? '今週' : '全体'}
-          </button>
-        ))}
-      </div>
-    );
-  }
+interface TabNavigationProps {
+	selectedTab: "today" | "weekly" | "allTime";
+	setSelectedTab: (tab: "today" | "weekly" | "allTime") => void;
+}
+
+export default function TabNavigation({
+	selectedTab,
+	setSelectedTab,
+}: TabNavigationProps) {
+	return (
+		<div className="w-4/5 flex space-x-2 p-1 bg-white shadow-md rounded-[29px]">
+			{["today", "weekly", "allTime"].map((tab) => (
+		<Button
+			variant="primary"
+			key={tab}
+			onClick={() => setSelectedTab(tab as "today" | "weekly" | "allTime")}
+			className={`flex-1 rounded-[29px] font-bold text-center text-[#333333] ${
+				selectedTab === tab ? "bg-orange-200" : "bg-white"
+			}`}
+		>
+			{tab === "today" ? "今日" : tab === "weekly" ? "今週" : "全体"}
+		</Button>
+			))}
+		</div>
+	);
+}
