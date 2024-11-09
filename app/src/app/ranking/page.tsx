1c1727f3d57d6f9dc21ad45c8297502fc2b34435
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-11-10 04:58:45 +0900

    [feat] Rankingページのデザインを変更

--- app/src/app/ranking/page.tsx
+++ app/src/app/ranking/page.tsx
@@ -1,27 +1,38 @@
-// app/ranking/page.tsx
 "use client";
-import TabNavigation from '../../components/view/ranking/TabNavigation';
-import TopicTabs from '../../components/view/ranking//TopicTabs';
-import RankingListToday from '../../components/view/ranking//RankingListToday';
-import RankingListWeekly from '../../components/view/ranking//RankingListWeekly';
-import RankingListAllTime from '../../components/view/ranking//RankingListAll';
-import { useState } from 'react';
+import { useState } from "react";
+import RankingListAllTime from "../../components/view/ranking//RankingListAll";
+import RankingListToday from "../../components/view/ranking//RankingListToday";
+import RankingListWeekly from "../../components/view/ranking//RankingListWeekly";
+import TopicTabs from "../../components/view/ranking//TopicTabs";
+import TabNavigation from "../../components/view/ranking/TabNavigation";
 
 export default function RankingPage() {
-  const [selectedTab, setSelectedTab] = useState<'today' | 'weekly' | 'allTime'>('today');
-  const [selectedTopic, setSelectedTopic] = useState(1);
+	const [selectedTab, setSelectedTab] = useState<
+		"today" | "weekly" | "allTime"
+	>("today");
+	const [selectedTopic, setSelectedTopic] = useState(1);
 
-  return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <TabNavigation selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-      {selectedTab === 'today' && (
-        <>
-          <TopicTabs selectedTopic={selectedTopic} setSelectedTopic={setSelectedTopic} />
-          <RankingListToday selectedTopic={selectedTopic} />
-        </>
-      )}
-      {selectedTab === 'weekly' && <RankingListWeekly />}
-      {selectedTab === 'allTime' && <RankingListAllTime />}
-    </div>
-  );
+	return (
+		<div className="min-h-screen bg-gradient-to-t from-gray-300 via-gray-200 to-gray-50 px-4">
+			<div className="w-full flex flex-col items-center justify-center sticky top-0 z-10 pt-4">
+				<TabNavigation
+					selectedTab={selectedTab}
+					setSelectedTab={setSelectedTab}
+				/>
+				{selectedTab === "today" && (
+					<TopicTabs
+						selectedTopic={selectedTopic}
+						setSelectedTopic={setSelectedTopic}
+					/>
+				)}
+			</div>
+			<div>
+				{selectedTab === "today" && (
+					<RankingListToday selectedTopic={selectedTopic} />
+				)}
+				{selectedTab === "weekly" && <RankingListWeekly />}
+				{selectedTab === "allTime" && <RankingListAllTime />}
+			</div>
+		</div>
+	);
 }
