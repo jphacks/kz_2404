2b45788b0df0f4231b7594caebc4252867cff052
hikahana <22.h.hanada.nutfes@gmail.com>
2024-11-12 14:02:00 +0000

    fix response data

--- app/src/app/api/assignment/today/route.ts
+++ app/src/app/api/assignment/today/route.ts
@@ -1,22 +1,22 @@
-;import { prisma } from "@lib/prisma";
 import type { Assignment, todayAssignment } from "@/types";
+import { prisma } from "@lib/prisma";
 
 // 課題新規作成（ランダム）
 export async function GET() {
 	const startOfToday = new Date();
 	startOfToday.setHours(0, 0, 0, 0); // 今日の開始時間を設定 (00:00:00)
-  
+
 	const endOfToday = new Date();
 	endOfToday.setHours(23, 59, 59, 999); // 今日の終了時間を設定 (23:59:59)
 
 	const assignments = await prisma.assignment.findMany({
 		where: {
 			date: {
-			  gte: startOfToday, // 今日の開始時間以上
-			  lte: endOfToday,   // 今日の終了時間以下
+				gte: startOfToday, // 今日の開始時間以上
+				lte: endOfToday, // 今日の終了時間以下
 			},
-		  },
-		  include: { word: true },
+		},
+		include: { word: true },
 	});
 	console.log(assignments);
 
@@ -24,17 +24,13 @@ export async function GET() {
 		const todayAssignment: todayAssignment = {
 			assignmentId: assignment.id,
 			english: assignment.word.english,
+			assignTime: assignment.date,
 		};
 		return todayAssignment;
-	})
+	});
 
 	return new Response(JSON.stringify(todayAssignments), {
 		status: 200,
 		headers: { "Content-Type": "application/json" },
 	});
 }
-
-
-
-
-
