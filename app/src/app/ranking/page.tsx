666744ac83022cb606cc03330deff86bcc63b0bb
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-11-12 22:30:24 +0900

    [fix] pageでデータを取得して渡すように修正

--- app/src/app/ranking/page.tsx
+++ app/src/app/ranking/page.tsx
@@ -1,16 +1,33 @@
 "use client";
-import { useState } from "react";
-import RankingListAllTime from "../../components/view/ranking//RankingListAll";
-import RankingListToday from "../../components/view/ranking//RankingListToday";
-import RankingListWeekly from "../../components/view/ranking//RankingListWeekly";
-import TopicTabs from "../../components/view/ranking//TopicTabs";
+import type { todayAssignment } from "@/types";
+import { useEffect, useState } from "react";
+import RankingListAllTime from "../../components/view/ranking/RankingListAll";
+import RankingListToday from "../../components/view/ranking/RankingListToday";
+import RankingListWeekly from "../../components/view/ranking/RankingListWeekly";
 import TabNavigation from "../../components/view/ranking/TabNavigation";
+import TopicTabs from "../../components/view/ranking/TopicTabs";
 
 export default function RankingPage() {
 	const [selectedTab, setSelectedTab] = useState<
 		"today" | "weekly" | "allTime"
 	>("today");
-	const [selectedTopic, setSelectedTopic] = useState(1);
+	const [selectedTopic, setSelectedTopic] = useState(0);
+	const [topics, setTopics] = useState<todayAssignment[]>([]);
+
+	useEffect(() => {
+		const fetchTopics = async () => {
+			try {
+				const response = await fetch("api/assignment/today");
+				const data: todayAssignment[] = await response.json();
+				setTopics(data);
+				setSelectedTopic(data[0].assignmentId);
+			} catch (error) {
+				console.error("Error fetching topics:", error);
+			}
+		};
+
+		fetchTopics();
+	}, []);
 
 	return (
 		<div className="min-h-screen bg-gradient-to-t from-gray-300 via-gray-200 to-gray-50 px-4">
@@ -23,6 +40,7 @@ export default function RankingPage() {
 					<TopicTabs
 						selectedTopic={selectedTopic}
 						setSelectedTopic={setSelectedTopic}
+						topics={topics}
 					/>
 				)}
 			</div>
--- app/src/components/view/ranking/TopicTabs.tsx
+++ app/src/components/view/ranking/TopicTabs.tsx
@@ -1,49 +1,36 @@
-// app/ranking/components/TopicTabs.tsx
-import { todayAssignment } from '@/types';
-import React, { useEffect, useState } from 'react';
+import { Button } from "@/components/ui/button";
+import type { todayAssignment } from "@/types";
+import type React from "react";
 
 interface TopicTabsProps {
-  selectedTopic: number;
-  setSelectedTopic: (topic: number) => void;
+	topics: todayAssignment[];
+	selectedTopic: number;
+	setSelectedTopic: (topic: number) => void;
 }
 
-const TopicTabs: React.FC<TopicTabsProps> = ({ selectedTopic, setSelectedTopic }) => {
-  const [topics, setTopics] = useState<todayAssignment[]>([]);
-  const today = new Date().toISOString().split('T')[0];
-
-  useEffect(() => {
-    const fetchTopics = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/api/assignment/today`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch topics');
-        }
-        const data: todayAssignment[] = await response.json();
-        console.log('Fetched topics:', data);
-        setTopics(data);
-      } catch (error) {
-        console.error('Error fetching topics:', error);
-      }
-    };
-
-    fetchTopics();
-  }, [today]);
-
-  return (
-    <div className="flex overflow-x-auto space-x-4 mt-4">
-      {topics.map((topic) => (
-        <button
-          key={topic.assignmentId}
-          onClick={() => setSelectedTopic(topic.assignmentId)}
-          className={`py-2 px-4 ${
-            selectedTopic === topic.assignmentId ? 'bg-orange-200 text-[#333333]' : 'bg-gray-200'
-          } rounded-full`}
-        >
-          {topic.english}
-        </button>
-      ))}
-    </div>
-  );
+const TopicTabs: React.FC<TopicTabsProps> = ({
+	selectedTopic,
+	setSelectedTopic,
+  topics,
+}) => {
+	return (
+		<div className="flex overflow-x-auto space-x-4 mt-4">
+			{topics.map((topic) => (
+				<Button
+					variant={"primary"}
+					key={topic.assignmentId}
+					onClick={() => setSelectedTopic(topic.assignmentId)}
+					className={`py-2 px-4 ${
+						selectedTopic === topic.assignmentId
+							? "bg-orange-200 text-[#333333]"
+							: "bg-gray-200"
+					} rounded-full`}
+				>
+					{topic.english}
+				</Button>
+			))}
+		</div>
+	);
 };
 
 export default TopicTabs;
