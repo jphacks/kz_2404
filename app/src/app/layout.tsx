43e9f5d17d7a8ffab6f377384f96b5752aa6dcbd
nose221834 <iatyy01151821@gmail.com>
2024-11-13 22:55:33 +0900

    設定情報を読み込み

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -5,30 +5,31 @@ import Footer from "@/components/layout/footer";
 import SessionProvider from "../provider/SessionProvider";
 
 const notoSansJP = Noto_Sans_JP({
-	weight: ["400", "700"],
-	subsets: ["latin"],
-	variable: "--font-noto-sans-jp",
-	display: "swap",
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-noto-sans-jp",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-	title: "Let`s pics",
-	description: "Let`s pics",
+  title: "Let`s pics",
+  description: "Let`s pics",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<SessionProvider>
-			<html lang="ja">
-				<body className={`${notoSansJP.className} min-h-screen flex flex-col`}>
-					<div className="flex-grow">{children}</div>
-					<Footer />
-				</body>
-			</html>
-		</SessionProvider>
-	);
+  return (
+    <SessionProvider>
+      <html lang="ja">
+        <link rel="manifest" href="/manifest.json" />
+        <body className={`${notoSansJP.className} min-h-screen flex flex-col`}>
+          <div className="flex-grow">{children}</div>
+          <Footer />
+        </body>
+      </html>
+    </SessionProvider>
+  );
 }
