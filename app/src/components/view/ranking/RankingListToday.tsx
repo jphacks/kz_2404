9308f31aa16956a0aee3ba814f2158163a1b4085
hikahana <22.h.hanada.nutfes@gmail.com>
2024-11-15 20:59:05 +0000

    fix Loading

--- app/src/components/view/ranking/RankingListToday.tsx
+++ app/src/components/view/ranking/RankingListToday.tsx
@@ -1,17 +1,11 @@
 import { Button } from "@/components/ui/button";
 import type { ScoreDetail } from "@/types";
-import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 import { LuClock } from "react-icons/lu";
 import { MdOutlineImageSearch } from "react-icons/md";
 import PhotoCameraIcon from "../../../../public/icons/icon-photo-camera.svg";
-
-const LoadingSpinner = () => (
-	<div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center z-50">
-		<div className="animate-spin rounded-full h-24 w-24 border-t-4 border-b-4 border-blue-500 mb-4" />
-		<p className="text-white text-lg">読み込み中...</p>
-	</div>
-);
+import { LoadingSpinner } from "../LoadingSpinner";
 
 const RankingListToday: React.FC<{ selectedTopic: number }> = ({
 	selectedTopic,
