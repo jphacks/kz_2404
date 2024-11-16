b8621674e284a053cd03b10995ea279638cf6ac7
nose221834 <iatyy01151821@gmail.com>
2024-11-16 15:18:39 +0900

    aタグをlinkに変更

--- app/src/components/layout/footer.tsx
+++ app/src/components/layout/footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import ThemeIcon from "../../../public/icons/icon-history.svg";
@@ -10,7 +11,6 @@ import UserIcon from "../../../public/icons/icon-user.svg";
 
 const Footer = () => {
 	const [activeButton, setActiveButton] = useState<string | null>(null);
-	const router = useRouter();
 	const pathname = usePathname();
 
 	useEffect(() => {
@@ -20,44 +20,44 @@ const Footer = () => {
 		else if (pathname === "/user") setActiveButton("user");
 	}, [pathname]);
 
-	const handleClick = (path: string) => {
-		router.push(path); // クライアントサイドでのページ遷移
-	};
-
 	return (
 		<footer className="w-full flex justify-evenly py-2 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] sticky bottom-0 bg-white z-10">
-			<Button
-				onClick={() => handleClick("/camera")}
-				variant={activeButton === "camera" ? "iconActive" : "iconDefault"}
-				className="flex flex-col items-center justify-center w-16 h-16"
-			>
-				<PhotoCameraIcon />
-				<div className="text-xs">撮影</div>
-			</Button>
-			<Button
-				onClick={() => handleClick("/")}
-				variant={activeButton === "theme" ? "iconActive" : "iconDefault"}
-				className="flex flex-col items-center justify-center w-16 h-16"
-			>
-				<ThemeIcon />
-				<div className="text-xs">お題</div>
-			</Button>
-			<Button
-				onClick={() => handleClick("/ranking")}
-				variant={activeButton === "ranking" ? "iconActive" : "iconDefault"}
-				className="flex flex-col items-center justify-center w-16 h-16"
-			>
-				<RankingIcon />
-				<div className="text-xs">ランキング</div>
-			</Button>
-			<Button
-				onClick={() => handleClick("/user")}
-				variant={activeButton === "user" ? "iconActive" : "iconDefault"}
-				className="flex flex-col items-center justify-center w-16 h-16"
-			>
-				<UserIcon />
-				<div className="text-xs">アカウント</div>
-			</Button>
+			<Link href="/camera">
+				<Button
+					variant={activeButton === "camera" ? "iconActive" : "iconDefault"}
+					className="flex flex-col items-center justify-center w-16 h-16"
+				>
+					<PhotoCameraIcon />
+					<div className="text-xs">撮影</div>
+				</Button>
+			</Link>
+			<Link href="/">
+				<Button
+					variant={activeButton === "theme" ? "iconActive" : "iconDefault"}
+					className="flex flex-col items-center justify-center w-16 h-16"
+				>
+					<ThemeIcon />
+					<div className="text-xs">お題</div>
+				</Button>
+			</Link>
+			<Link href="/ranking">
+				<Button
+					variant={activeButton === "ranking" ? "iconActive" : "iconDefault"}
+					className="flex flex-col items-center justify-center w-16 h-16"
+				>
+					<RankingIcon />
+					<div className="text-xs">ランキング</div>
+				</Button>
+			</Link>
+			<Link href="/user">
+				<Button
+					variant={activeButton === "user" ? "iconActive" : "iconDefault"}
+					className="flex flex-col items-center justify-center w-16 h-16"
+				>
+					<UserIcon />
+					<div className="text-xs">アカウント</div>
+				</Button>
+			</Link>
 		</footer>
 	);
 };
