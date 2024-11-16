bcddcdfccba14aeee32c68689ce9618bfe6a80f5
nose221834 <iatyy01151821@gmail.com>
2024-11-16 18:35:25 +0900

    名前を変更するAPIを作成

---
+++ app/src/app/api/user/rename/[id]/route.ts
@@ -0,0 +1,38 @@
+import { prisma } from "@lib/prisma";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+
+export async function PUT(req: NextRequest) {
+	const { pathname } = new URL(req.url || "");
+	const id = Number(pathname.split("/").pop());
+
+	if (!id) {
+		return NextResponse.json(
+			{ error: "Invalid or missing ID" },
+			{ status: 400 },
+		);
+	}
+
+	try {
+		const { name } = await req.json();
+		if (typeof name !== "string") {
+			return NextResponse.json(
+				{ error: "Invalid name value" },
+				{ status: 400 },
+			);
+		}
+
+		const putName = await prisma.user.update({
+			where: { id },
+			data: { name : name },
+		});
+
+		return NextResponse.json({ putName }, { status: 200 });
+	} catch (error) {
+		return NextResponse.json(
+			{ error: "Failed to update name", details: error },
+			{ status: 500 },
+		);
+	}
+}
